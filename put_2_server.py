97d003445e2d595423116a90b4b2d7557216b060
Dawi Lu <dawi9840@gmail.com>
2024-08-02 16:09:50 +0800

    Add files via upload

---
+++ put_2_server.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def put_can_status(can_status: str):  
+    can_url = "http://20.78.3.60:8080/version/can"  # URL for PUT request
+
+    data = {'status' : can_status}
+
+    # 發送 PUT 請求以發送數據
+    response = requests.put(can_url, json=data)
+
+    # 檢查請求是否成功
+    if response.status_code == 200:
+        print(f"Data posted successfully: {data}")
+    else:
+        print(f"Failed to post data: {response.status_code}, {response.text}")
+
+if __name__ == '__main__':
+    # Input string 0 or 1
+    put_can_status('0')
