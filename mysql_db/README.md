7095f448567b0a76b39d418139c7d15b2a17c2c1
root <root@ebg-hhtd.usuzdzitagnuvkesoxzzaxbdwa.lx.internal.cloudapp.net>
2024-08-29 03:56:35 +0000

    Add MySQL DB backup sql files

---
+++ mysql_db/README.md
@@ -0,0 +1,55 @@
+# This is MySQL backup database files.   
+
+## 登錄到 MySQL，系統會提示你輸入 root 用戶的密碼。   
+$: sudo mysql -u root -p   
+f********3    
+
+## 查詢用戶，進入 MySQL 命令行後，使用以下 SQL 查詢來列出所有用戶    
+$: SELECT User, Host FROM mysql.user;    
++------------------+-----------+    
+| User             | Host      |    
++------------------+-----------+    
+| debian-sys-maint | localhost |    
+| mysql.infoschema | localhost |    
+| mysql.session    | localhost |    
+| mysql.sys        | localhost |    
+| root             | localhost |    
++------------------+-----------+    
+5 rows in set (0.00 sec)    
+
+## 查詢數據庫名稱    
+$: SHOW DATABASES;    
++--------------------+    
+| Database           |     
++--------------------+    
+| dawidb             |    
+| information_schema |    
+| mysql              |    
+| performance_schema |    
+| sys                |    
+| userdb             |    
+| versiondb          |   
++--------------------+   
+7 rows in set (0.01 sec)  
+
+## 退出 MySQL  
+$: EXIT;  
+
+## mysqldump 是一個外部命令，不能直接在 MySQL 交互式命令行界面中執行。  
+## 在系統的終端機中執行 mysqldump 命令，而不是在 MySQL 的命令行中。  
+
+## 備份 其中的一個 MySQL 數據庫（例如 dawidb）  
+$: mysqldump -u root -p dawidb > /path/to/backup_dawidb.sql  
+f********3   
+
+## 恢復數據庫   
+## 如果要在新的 MySQL 環境中恢復數據庫，可使用：   
+
+## 1.創建數據庫（如果尚未創建）   
+$: sudo mysql -u root -p   
+$: CREATE DATABASE dawidb;  
+$: EXIT;  
+
+## 2.恢復數據庫   
+$: mysql -u root -p dawidb < /path/to/backup_dawidb.sql 
+ 
---
+++ mysql_db/backup_versiondb.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
+--
+-- Host: localhost    Database: versiondb
+-- ------------------------------------------------------
+-- Server version	8.0.39-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `versions`
+--
+
+DROP TABLE IF EXISTS `versions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `versions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `versions`
+--
+
+LOCK TABLES `versions` WRITE;
+/*!40000 ALTER TABLE `versions` DISABLE KEYS */;
+INSERT INTO `versions` VALUES (1,'can','0'),(2,'kuma','1'),(3,'dawi','1'),(4,'status','100'),(5,'hhtd24','011'),(6,'hhtd24','011');
+/*!40000 ALTER TABLE `versions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-29  3:46:10
